ab9780577dcba682ea9c7ccb34c2342fb8b32346
mijara <marjara35@gmail.com>
2016-10-22 20:38:47 -0300

    Fix some links.

--- adn3/news/models.py
+++ adn3/news/models.py
@@ -4,11 +4,15 @@ from django.db import models
 
 class New(models.Model):
     course = models.ForeignKey('courses.Course', verbose_name=u'Curso')
+
     title = models.CharField(max_length=512, verbose_name=u'Título')
+
     body = models.TextField(verbose_name=u'Cuerpo')
+
     owner = models.ForeignKey('auth.User')
 
     create_date = models.DateTimeField(auto_now_add=True)
+
     update_date = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
