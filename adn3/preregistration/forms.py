bfde4f7d18fac0c8af8e00a74a5bb95fed8b8c25
mijara <marjara35@gmail.com>
2017-06-09 22:10:31 -0400

    Preregistrations overhaul

--- adn3/preregistration/forms.py
+++ adn3/preregistration/forms.py
@@ -3,18 +3,12 @@ from .models import *
 
 
 class PreRegistrationForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(PreRegistrationForm, self).__init__(*args, **kwargs)
-        agendas = self.initial['course'].agenda_set
-
-        self.fields['first_preference'].queryset = agendas
-        self.fields['second_preference'].queryset = agendas
-        self.fields['third_preference'].queryset = agendas
-
     class Meta:
         model = PreRegistration
         fields = [
             'first_preference',
             'second_preference',
-            'third_preference'
+            'third_preference',
+            'fourth_preference',
+            'fifth_preference',
         ]
---
+++ adn3/preregistration/templatetags/preregistrations.py
@@ -0,0 +1,40 @@
+from django import template
+from django.template import Node
+
+register = template.Library()
+
+
+class CalendarNode(Node):
+    def render(self, context):
+        days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes']
+        blocks = ['1 - 2', '3 - 4', '5 - 6', '7 - 8', '9 - 10', '11 - 12', '13 - 14']
+
+        html = '<table class="table">\n'
+        html += '<thead>\n'
+        html += '<tr class="table-heading">\n'
+        html += '<th width="15%">Bloque</th>\n'
+
+        for day in days:
+            html += '<th width="17%" class="text-center">{}</th>'.format(day)
+
+        html += '</tr>'
+        html += '</thead>'
+        html += '<tbody>'
+
+        for block in blocks:
+            html += '<tr>'
+            html += '<td>{block}</td>'.format(block=block)
+
+            for day in days:
+                html += '<td><div class="calendar-item">{day}<br>' \
+                        '<div>{block}</div></div></td>'.format(day=day, block=block)
+
+        html += '</tbody>'
+        html += '</table>'
+
+        return html
+
+
+@register.tag
+def generate_calendar(value, arg):
+    return CalendarNode()
