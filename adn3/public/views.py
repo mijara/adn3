0270fd06ebd9b92d14aeef57d08eb401f2246725
Hernán Herreros Niño <hernan.herreros.13@sansano.usm.cl>
2017-06-10 01:08:35 -0400

    Add format to the news in the student section

--- adn3/public/views.py
+++ adn3/public/views.py
@@ -45,17 +45,11 @@ class CourseDetail(generic.DetailView):
             test_as_dict['time'] = test.get_timeout_display
             test_as_dict['session'] = test.session.number
             test_as_dict['pk'] = test.pk
-
-            for version in test.version_set.all():
-                try:
-                    student_version = StudentsAnswers.objects.get(student=self.request.user, version=version)
-                except ObjectDoesNotExist:
-                    student_version = None
-
-                if student_version:
-                    test_as_dict['status'] = student_version.get_status()
-                elif not student_version:
-                    test_as_dict['status'] = 0
+            try:
+                sv = StudentsAnswers.objects.get(student=self.request.user, version__test=test)
+                test_as_dict['status'] = sv.get_status()
+            except ObjectDoesNotExist:
+                test_as_dict['status'] = 0
 
             context['test_set'].append(test_as_dict)
         return context
@@ -67,41 +61,41 @@ class TestPreConfirmationView(generic.DetailView):
 
     def get(self, request, *args, **kwargs):
         test = self.get_object()
-        student_version = StudentsAnswers.objects.filter(student=self.request.user)
-
-        for sv in student_version:
-            if sv.version.test == test and sv.get_status() == 2:
+        try:
+            sv = StudentsAnswers.objects.get(student=self.request.user, version__test=test)
+            if sv.get_status() == 2:
                 return HttpResponseRedirect(reverse('public:course_detail', kwargs={'pk': test.course.pk}))
-            elif sv.version.test == test and sv.get_status() == 1:
+            elif sv.get_status() == 1:
                 return HttpResponseRedirect(reverse('public:test_detail', kwargs={'pk': sv.version.pk}))
 
-        else:
-            return super().get(self, request, *args, **kwargs)
+        except ObjectDoesNotExist:
+            pass
 
+        return super().get(self, request, *args, **kwargs)
 
 class TestVersionAssignView(generic.DetailView):
     model = Test
 
     def get(self, request, *args, **kwargs):
         test = self.get_object()
-        student_version = StudentsAnswers.objects.filter(student=self.request.user)
-
-        for sv in student_version:
-            if sv.version.test == test and sv.get_status() == 2:
+        try:
+            sv = StudentsAnswers.objects.get(student=self.request.user, version__test=test)
+            if sv.get_status() == 2:
                 return HttpResponseRedirect(reverse('public:course_detail', kwargs={'pk': test.course.pk}))
-            elif sv.version.test == test and sv.get_status() == 1:
+            elif sv.get_status() == 1:
                 return HttpResponseRedirect(reverse('public:test_detail', kwargs={'pk': sv.version.pk}))
-        else:
+        except ObjectDoesNotExist:
             version = services.assign_version(test)
 
-            student_version = StudentsAnswers(student=request.user, version=version)
-            student_version.save()
+            sv = StudentsAnswers(student=request.user, version=version)
+            sv.save()
 
             # Once the students has a assigned version, create empty answers
             for question in version.question_set.all():
                 services.create_empty_answers(question, request.user)
 
             return HttpResponseRedirect(reverse('public:test_detail', kwargs={'pk': version.pk}))
+
 
 
 class TestDetailView(generic.DetailView):
@@ -114,7 +108,7 @@ class TestDetailView(generic.DetailView):
             context['studentanswer'] = StudentsAnswers.objects.get(student=self.request.user,
                                                                    version=self.object)
         except ObjectDoesNotExist:
-            return HttpResponseRedirect(reverse('public:course_detail', kwargs={'pk': version.test.course.pk}))
+            pass
 
         context['answers'] = Answer.objects.filter(student=self.request.user, question__version=self.get_object())
         return context
@@ -122,14 +116,11 @@ class TestDetailView(generic.DetailView):
     def get(self, request, *args, **kwargs):
         version = self.get_object()
         try:
-            student_version = StudentsAnswers.objects.get(student=self.request.user, version=version)
+            sv = StudentsAnswers.objects.get(student=self.request.user, version=version)
+            if sv.get_status() == 2:
+                return HttpResponseRedirect(reverse('public:course_detail', kwargs={'pk': version.test.course.pk}))
         except ObjectDoesNotExist:
-            student_version = None
-
-        if student_version and student_version.get_status() == 2:
-            return HttpResponseRedirect(reverse('public:course_detail', kwargs={'pk': version.test.course.pk}))
-        elif not student_version:
-            return HttpResponseRedirect(reverse('public:preconfirmation_test', kwargs={'pk': version.test.pk}))
+            return HttpResponseRedirect(reverse('public:test_preconfirmation', kwargs={'pk': version.test.pk}))
 
         return super().get(self, request, *args, **kwargs)
 
@@ -137,8 +128,8 @@ class TestDetailView(generic.DetailView):
 class UpdateAnswers(View):
     def post(self, request):
         try:
-            student_answer = StudentsAnswers.objects.get(student=request.user, version__pk=request.POST['version'])
-            if student_answer.get_status() == 1:
+            sv = StudentsAnswers.objects.get(student=request.user, version__pk=request.POST['version'])
+            if sv.get_status() == 1:
                 for key in request.POST:
                     services.update_answer(key, request.user, request.POST[key], request.POST['version'])
 
@@ -146,13 +137,13 @@ class UpdateAnswers(View):
                     services.update_document(request.POST['version'], request.user, request.FILES[key])
 
                 # Update the field 'last_update'
-                student_answer.refresh_from_db()
-                student_answer.last_update = timezone.now()
-                student_answer.save()
+                sv.refresh_from_db()
+                sv.last_update = timezone.now()
+                sv.save()
                 return JsonResponse({
                     'error': False,
                     'message': timezone.localtime(timezone.now()).strftime('%H:%M'),
-                    'document': student_answer.document.name
+                    'document': sv.document.name
                 })
 
             else:
