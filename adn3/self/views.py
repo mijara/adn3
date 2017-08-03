39a8e916b8d08b3c9ea3b43a428c18600947f817
mijara <marjara35@gmail.com>
2017-08-03 12:52:18 -0400

    Filter teacher courses and divide current from old ones.

--- adn3/self/views.py
+++ adn3/self/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
+from adn3.constants import YEAR, SEMESTER
 from courses.models import *
 from django.views import generic
 
@@ -9,3 +10,11 @@ from django.views import generic
 class CourseListView(generic.ListView):
     model = Course
     template_name = 'self/teachers/course_list.html'
+
+    def get_queryset(self):
+        return self.request.user.course_set.filter(year=YEAR, semester=SEMESTER)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['old_course_list'] = self.request.user.course_set.exclude(year=YEAR, semester=SEMESTER)
+        return context
