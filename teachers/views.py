52fb9188841f7d72fe5f1d9e4ecf0fb69fc4952c
Marcelo Jara <marjara35@gmail.com>
2017-09-07 20:54:31 -0300

    Coordinator can see all current courses.

--- teachers/views.py
+++ teachers/views.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from adn3.services import is_teacher, is_student
-
+from adn3.services import is_teacher, is_student, is_coordinator
 
 from adn3.constants import YEAR, SEMESTER
 from courses.models import *
@@ -15,6 +14,8 @@ class CourseListView(UserPassesTestMixin, generic.ListView):
     template_name = 'teachers/course_list.html'
 
     def get_queryset(self):
+        if is_coordinator(self.request.user):
+            return Course.objects.filter(year=YEAR, semester=SEMESTER)
         return self.request.user.course_set.filter(year=YEAR, semester=SEMESTER)
 
     def get_context_data(self, **kwargs):
