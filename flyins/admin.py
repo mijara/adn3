176b3731dd9809263438de2efff4e4507a0d710a
hrherrer <hernan.herreros.13@sansano.usm.cl>
2018-03-13 23:20:09 -0300

    Add a search field in the flyin admin and add a contact email

--- flyins/admin.py
+++ flyins/admin.py
@@ -4,6 +4,7 @@ from .models import FlyIn
 
 class FlyInAdmin(admin.ModelAdmin):
     list_display = ('get_full_name', 'rol', 'course', 'software')
+    search_fields = ('first_name', 'last_names', 'rol')
 
 
 admin.site.register(FlyIn, FlyInAdmin)
