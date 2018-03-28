4b2dbbab6715eae6d2ca03624d0c5507edbf10fb
mijara <marjara35@gmail.com>
2018-03-28 11:55:59 -0300

    Add search field for students in django admin

--- registration/admin.py
+++ registration/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ('user__first_name', 'user__email', 'rol')
+
+    list_display = ('user', 'email', 'rol', 'campus')
+
+    def email(self, obj):
+        return obj.user.email
+
+
+admin.site.register(Student, StudentAdmin)
 admin.site.register(Ticket)
