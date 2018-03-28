4dbc0ad076f48e7ba9e7c57d1f4fda92f194bad1
mijara <marjara35@gmail.com>
2018-03-28 12:02:58 -0300

    Fix search field for students

--- registration/admin.py
+++ registration/admin.py
@@ -3,12 +3,15 @@ from .models import *
 
 
 class StudentAdmin(admin.ModelAdmin):
-    search_fields = ('user__first_name', 'user__email', 'rol')
+    search_fields = ('user__first_name', 'user__last_name', 'user__email', 'rol')
 
-    list_display = ('user', 'email', 'rol', 'campus')
+    list_display = ('full_name', 'email', 'rol', 'campus')
 
     def email(self, obj):
         return obj.user.email
+
+    def full_name(self, obj):
+        return obj.user.get_full_name()
 
 
 admin.site.register(Student, StudentAdmin)
