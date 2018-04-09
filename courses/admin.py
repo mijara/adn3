d8917536ef8225a1b47ca075a48b7972c7e407e6
mijara <marjara35@gmail.com>
2018-04-09 01:27:23 -0300

    Add aditional data to agenda admin view

--- courses/admin.py
+++ courses/admin.py
@@ -11,7 +11,20 @@ class CourseAdmin(admin.ModelAdmin):
     inlines = (CourseTeacherInline,)
 
 
+class AgendaAdmin(admin.ModelAdmin):
+    list_display = ('day', 'block', 'room_name', 'course_name', 'campus', 'software')
+
+    def room_name(self, object):
+        return object.room.name
+
+    def course_name(self, object):
+        return object.course.name
+
+    def campus(self, object):
+        return object.course.campus.name
+
+
 admin.site.register(Room)
 admin.site.register(Campus)
 admin.site.register(Course, CourseAdmin)
-admin.site.register(Agenda)
+admin.site.register(Agenda, AgendaAdmin)
